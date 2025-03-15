0c33982ab902af5f6b31b83c14cc0537b3285786
PandolfiJoaquin <jpandolfi@fi.uba.ar>
2025-03-15 20:59:55 -0300

    Fix script

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,7 +1,7 @@
 #build image for script
 docker build -f ./validador-server/Dockerfile -t "validator:latest" ./validador-server
 #run container, script, and check exit code
-docker run --network tp0_testing_net validator:latest 2>&1 ./validador-server/docker-run-logs.txt
+docker run --network tp0_testing_net validator:latest
 exit_code=$?
 if [ $exit_code -eq 0 ]; then
    echo "action: test_echo_server | result: success"
