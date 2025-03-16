6a800100be3f8ad7a330f60eecb9e548827be0c4
PandolfiJoaquin <jpandolfi@fi.uba.ar>
2025-03-15 21:39:06 -0300

    Fix script

--- validador-server/netcat_script.sh
+++ validador-server/netcat_script.sh
@@ -1,6 +1,6 @@
 echo "Starting health-check"
 
-if [ "$(echo "ping" | nc -w server 12345)" = "ping" ]; then
+if [ "$(echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" | nc -w server 12345)" = "ping" ]; then
     exit 0
 fi
 exit 1
