5e91716b35aa788187f71a331dfdc549f2897c75
PandolfiJoaquin <jpandolfi@fi.uba.ar>
2025-03-26 19:43:22 -0300

    Fix closing of client socket

--- server/common/server.py
+++ server/common/server.py
@@ -11,6 +11,8 @@ class Server:
         signal.signal(signal.SIGINT, self.__sigterm_handler)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self.running = False
+        self.skt = None
 
     def run(self):
         """
@@ -25,6 +27,7 @@ class Server:
         while self.running:
             
             client_sock = self.__accept_new_connection()
+            self.skt = client_sock
             self.__handle_client_connection(client_sock)
 
     def __handle_client_connection(self, client_sock):
@@ -43,9 +46,6 @@ class Server:
             client_sock.send("{}\n".format(msg).encode('utf-8'))
         except OSError as e:
             logging.error("action: receive_message | result: fail | error: {e}")
-        finally:
-            client_sock.shutdown(socket.SHUT_WR)
-            client_sock.close()
 
     def __accept_new_connection(self):
         """
@@ -64,5 +64,8 @@ class Server:
     def __sigterm_handler(self, sig, frame):
         self._server_socket.shutdown(socket.SHUT_RDWR)
         self._server_socket.close()
+        self.skt.shutdown(socket.SHUT_RDWR)
+        self.skt.close()
+
         self.running = False
         exit(0)
